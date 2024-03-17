46256eb844adb620ed40afddd967c0694dfd2c48
Elian <elianpullen@protonmail.com>
2024-03-17 14:09:16 +0100

    style: weight.create, index blade
    Dark mode implemented

--- resources/views/weight/create.blade.php
+++ resources/views/weight/create.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Graph(s)') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
 
                 @php
                     $labels = [];
@@ -25,7 +25,7 @@
                 @endforeach
 
                 <div class="shadow-lg rounded-lg overflow-hidden">
-                    <div class="py-3 px-5 bg-gray-50">Weight(KG)</div>
+                    <div class="py-3 px-5 bg-gray-600">Weight(KG)</div>
                     <canvas class="p-10" id="chartLine"></canvas>
                 </div>
 
