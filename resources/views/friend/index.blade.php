8da00597084bbb6623bb96f535319de464784e79
Elian <elianpullen@protonmail.com>
2024-03-22 23:52:46 +0100

    feat: improved friend functionality
    renamed weight.create header name

--- resources/views/friend/index.blade.php
+++ resources/views/friend/index.blade.php
@@ -7,14 +7,19 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl rounded-t-lg text-white">
+            <div class="bg-white dark:bg-gray-800 shadow-xl rounded-t-lg text-white">
                 @foreach ($users as $user)
                     <div class="p-4">
-                        <p>{{$user->name}}</p>
-                        <a href="{{ url('/friend/' . $user->id . '/add') }}"
-                           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Add</a>
-                        <a href="{{ url('/friend/' . $user->id . '/remove') }}"
-                           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Remove</a>
+                        <p class="py-2">{{$user->name}}</p>
+                        @if(\Illuminate\Support\Facades\Auth::user()->friends->contains($user->id))
+
+                            <a href="{{ url('/friend/' . $user->id . '/remove') }}"
+                               class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Remove</a>
+                        @else
+                            <a href="{{ url('/friend/' . $user->id . '/add') }}"
+                               class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Add</a>
+                        @endif
+
                     </div>
                 @endforeach
             </div>
