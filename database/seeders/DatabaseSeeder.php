3290f066fcb121c25a11f88d988a127ebd42a816
Elian <elianpullen@protonmail.com>
2024-03-27 14:47:27 +0100

    feat: user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,17 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Gerjan',
-            'email' => 'gerjan@gerjan.nl',
-            'password' => Hash::make('password'),
-        ]);
-
-        User::create([
-            'name' => 'Arnout',
-            'email' => 'arnout@arnout.nl',
-            'password' => Hash::make('password'),
-        ]);
-        ($this->call(WeightDataSeeder::class));
+        ($this->call([
+            WeightDataSeeder::class,
+            UserSeeder::class,
+        ]));
     }
 }
