63faf4c7ca9b5e22bc1f14c7d3279e56406d47d5
Elian <elianpullen@protonmail.com>
2024-04-22 23:18:58 +0200

    feat: add weightData routes and controllers

--- app/Http/Controllers/WeightDataController.php
+++ app/Http/Controllers/WeightDataController.php
@@ -9,21 +9,28 @@ use Illuminate\Support\Facades\Auth;
 
 class WeightDataController extends Controller
 {
-    public function index()
-    {
-        $auth = Auth::user();
-
-        $weights = $auth->weights()->with('user')->orderBy('date', 'asc')->get();
-        return view('weight.index', compact('weights'));
-    }
     public function graph()
     {
         $auth = Auth::user();
 
-        $weights = $auth->weights()->with('user')->orderBy('date', 'asc')->get();
+        $weights = $auth->weights()->with('user')->orderBy('date', 'asc')->get(); // Oldest to newest
         return view('weight.graph', compact('weights'));
     }
 
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $auth = Auth::user();
+
+        $weights = $auth->weights()->with('user')->orderBy('date', 'desc')->get(); // newest to oldest
+        return view('weight.index', compact('weights'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
         $auth = Auth::user();
@@ -43,13 +50,9 @@ class WeightDataController extends Controller
         return view('weight.create', compact('weight', 'date', 'bodyfat'));
     }
 
-    public function edit(weightData $weightData)
-    {
-        $auth = Auth::user();
-
-
-    }
-
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(Request $request)
     {
         $auth = Auth::user();
@@ -61,5 +64,56 @@ class WeightDataController extends Controller
             'bodyfat' => $request->input('bodyfat') ?? 0,
         ]);
         return redirect('/weight/create');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(weightDummyData $weightDummyData)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($weightData)
+    {
+        $weight = weightData::find($weightData);
+
+        return view('weight.edit', compact('weight'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+
+    public function update(Request $request, $weightId)
+    {
+        $auth = auth()->user();
+
+        $weights = $auth->weights()->with('user')->orderBy('date', 'desc')->get(); // newest to oldest
+
+        $weightData = weightData::find($weightId);
+        $weightData->date = $request->input('date');
+        $weightData->weight = $request->input('weight');
+        $weightData->bodyfat = $request->input('bodyfat') ?? 0;
+        $weightData->save();
+
+        return view('weight.index', compact('weights'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($weightId)
+    {
+        $auth = Auth::user();
+
+        $weight = weightData::select()
+            ->where('user_id', $auth->id)
+            ->where('id', $weightId);
+        $weight->delete();
+        return redirect('/weight');
     }
 }
