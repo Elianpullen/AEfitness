bd6870302d55cd4240f90ed8ffa44086d7567f22
Elian <elianpullen@protonmail.com>
2024-04-26 14:36:51 +0200

    refactor: friend.index page

--- resources/views/friend/index.blade.php
+++ resources/views/friend/index.blade.php
@@ -4,75 +4,41 @@
             {{ __('Friends') }}
         </h2>
     </x-slot>
-    {{--All users--}}
+    {{--User search--}}
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-gray-900">
                 <div class="mx-auto max-w-7xl">
                     <div class="bg-gray-800 py-10 rounded-lg">
                         <div class="px-4 sm:px-6 lg:px-8">
-                            <div class="sm:flex sm:items-center">
-                                <div class="sm:flex-auto">
-                                    <h1 class="text-base font-semibold leading-6 text-white">All users</h1>
-                                    <p class="mt-2 text-sm text-gray-300">A list of all the users.</p>
-                                </div>
-                                <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none text-white">
-                                    <p>test</p>
-                                </div>
+                            <div class="sm:flex sm:items-center justify-center">
+                                <form class="max-w-md mx-auto" action="{{ url('/friend/search') }}" method="GET">
+                                    <label for="default-search"
+                                           class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                                    <div class="relative">
+                                        <div
+                                            class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                                <path stroke="currentColor" stroke-linecap="round"
+                                                      stroke-linejoin="round" stroke-width="2"
+                                                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                            </svg>
+                                        </div>
+                                        <input type="search" id="default-search"
+                                               class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                               placeholder="Search user" required/>
+                                        <button type="submit"
+                                                class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                            Search
+                                        </button>
+                                    </div>
+                                </form>
                             </div>
                             <div class="mt-8 flow-root">
                                 <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                                     <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                                        <table class="min-w-full divide-y divide-gray-700">
-                                            <thead>
 
-                                            <tr>
-                                                <th scope="col"
-                                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0">
-                                                    #
-                                                </th>
-                                                <th scope="col"
-                                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0">
-                                                    Name
-                                                </th>
-                                                <th scope="col"
-                                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0">
-                                                    Email
-                                                </th>
-                                                <th scope="col"
-                                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0">
-                                                    Created_at
-                                                </th>
-                                                <th scope="col"
-                                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0">
-                                                    Updated_at
-                                                </th>
-                                            </tr>
-                                            </thead>
-                                            <tbody class="divide-y divide-gray-800">
-                                            @if ($users->count() == 0)
-                                                <tr>
-                                                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
-                                                        There are no others.
-                                                    </td>
-                                                </tr>
-                                            @else
-                                                @foreach ($users as $user)
-                                                    <tr>
-                                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">{{$user->id}}</td>
-                                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">{{$user->name}}</td>
-                                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">{{$user->email}}</td>
-                                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">{{$user->created_at}}</td>
-                                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">{{$user->updated_at}}</td>
-                                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
-                                                            <a href="{{ url('/friend/' . $user->id . '/request') }}"
-                                                               class="bg-orange-600 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded-full">Friend
-                                                                request</a></td>
-                                                    </tr>
-                                                @endforeach
-                                            @endif
-                                            </tbody>
-                                        </table>
                                     </div>
                                 </div>
                             </div>
