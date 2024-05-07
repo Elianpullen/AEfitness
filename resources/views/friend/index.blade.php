4d907ea7a668ed2470bbeea9f2f412243d77910a
Elian <elianpullen@protonmail.com>
2024-05-07 18:59:33 +0200

    refactor(search): improve index view, upload images

--- resources/views/friend/index.blade.php
+++ resources/views/friend/index.blade.php
@@ -4,6 +4,7 @@
             {{ __('Friends') }}
         </h2>
     </x-slot>
+
     {{--User search--}}
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -25,9 +26,11 @@
                                                       d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                             </svg>
                                         </div>
-                                        <input type="search" name="username"
-                                               class="block w-full p-4 mr-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                               placeholder="Search user" required/>
+                                        <label>
+                                            <input type="search" name="username"
+                                                   class="block w-full p-4 mr-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                   placeholder="Search user"/>
+                                        </label>
                                         <button type="submit"
                                                 class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                             Search
@@ -35,36 +38,133 @@
                                     </div>
                                 </form>
                             </div>
-                            {{--                            {{$results = session('results')}}--}}
-                            {{--                            {{$results = session('results')}}--}}
-                            {{--                            @dd($result = session()->only(['key']));--}}
-                            {{--                            {{$results = session()->get('test')}}--}}
-                            {{--                            {{$test = session()->only(['users'])}}--}}
-                            {{--                            {{$results = session()}}--}}
                             @isset ($results)
                                 @foreach($results as $result)
-                                    {{--                                    @dd($result)--}}
-                                    <ul role="list" class="divide-y divide-gray-100">
-                                        <li class="flex items-center justify-between gap-x-6 py-5">
-                                            <div class="flex min-w-0 gap-x-4">
-                                                {{--                                                <img class="h-12 w-12 flex-none rounded-full bg-gray-50"--}}
-                                                {{--                                                     src="{{$result->profile_photo_path}}"--}}
-                                                {{--                                                     alt="">--}}
-                                                <div class="min-w-0 flex-auto">
-                                                    <p class="text-sm font-semibold leading-6 text-gray-900">{{$result->name}}</p>
-                                                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{$result->email}}</p>
+                                    <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 py-2">
+                                        <li class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+                                            <div class="flex w-full items-center justify-between space-x-6 p-6">
+                                                <div class="flex-1 truncate">
+                                                    <div class="flex items-center space-x-3">
+                                                        <h3 class="truncate text-sm font-medium text-gray-900">{{$result->name}}</h3>
+                                                        @if(!$result->weights->isEmpty())
+                                                            <span
+                                                                class="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                                                            {{$result->weights->sortBy('date')->last()->weight}}kg</span>
+                                                        @endif
+                                                        @if(!$result->weights->isEmpty())
+                                                            <span
+                                                                class="inline-flex flex-shrink-0 items-center rounded-full bg-red-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-red-600/20">
+                                                                {{$result->weights->sortBy('date')->last()->bodyfat}}%</span>
+                                                        @endif
+                                                    </div>
+                                                    <p class="mt-1 truncate text-sm text-gray-500">Lorem ipsum</p>
                                                 </div>
+                                                @if(empty($result->profile_photo_path))
+                                                    <img class="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300"
+                                                         src="{{@asset('/images/user.png')}}" alt="userImage">
+
+                                                @else
+                                                    <img class="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300"
+                                                         src="{{@asset($result->profile_photo_path)}}" alt="userImage">
+                                                @endif
                                             </div>
-                                            <a href="{{ url('/friend/' . $result->id . '/request') }}"
-                                               class="rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Add</a>
+                                            <div class="-mt-px flex divide-x divide-gray-200">
+                                                {{--if user is already friends--}}
+                                                @if($friends->contains('user_id', $result->id) or $friends->contains('friend_id', $result->id))
+                                                    <div class="flex w-0 flex-1">
+                                                        <a href="{{ url('/friend/' . $result->id . '/remove') }}"
+                                                           class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                                                 height="24" viewBox="0 0 24 24" fill="none"
+                                                                 stroke="currentColor" stroke-width="2"
+                                                                 stroke-linecap="round" stroke-linejoin="round"
+                                                                 class="icon icon-tabler icons-tabler-outline icon-tabler-user-minus">
+                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"/>
+                                                                <path
+                                                                    d="M6 21v-2a4 4 0 0 1 4 -4h4c.348 0 .686 .045 1.009 .128"/>
+                                                                <path d="M16 19h6"/>
+                                                            </svg>
+                                                            Remove
+                                                        </a>
+                                                    </div>
+                                                @elseif($friendRequestsSend->contains('receiver_id', $result->id) and $friendRequestsSend->contains('status', 'pending'))
+                                                    <div class="-ml-px flex w-0 flex-1">
+                                                        <a href="{{ url('/friend/' . $result->id . '/remove') }}"
+                                                           class="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                                                 height="24" viewBox="0 0 24 24" fill="none"
+                                                                 stroke="currentColor" stroke-width="2"
+                                                                 stroke-linecap="round" stroke-linejoin="round"
+                                                                 class="icon icon-tabler icons-tabler-outline icon-tabler-user-plus">
+                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"/>
+                                                                <path d="M16 19h6"/>
+                                                                <path d="M19 16v6"/>
+                                                                <path d="M6 21v-2a4 4 0 0 1 4 -4h4"/>
+                                                            </svg>
+                                                            Decline
+                                                        </a>
+                                                    </div>
+                                                @elseif($friendRequestsReceived->contains('sender_id', $result->id) and $friendRequestsReceived->contains('status', 'pending'))
+                                                    <div class="flex w-0 flex-1">
+                                                        <a href="{{ url('/friend/' . $result->id . '/accept') }}"
+                                                           class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                                                 height="24" viewBox="0 0 24 24" fill="none"
+                                                                 stroke="currentColor" stroke-width="2"
+                                                                 stroke-linecap="round" stroke-linejoin="round"
+                                                                 class="icon icon-tabler icons-tabler-outline icon-tabler-user-minus">
+                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"/>
+                                                                <path
+                                                                    d="M6 21v-2a4 4 0 0 1 4 -4h4c.348 0 .686 .045 1.009 .128"/>
+                                                                <path d="M16 19h6"/>
+                                                            </svg>
+                                                            Accept
+                                                        </a>
+                                                    </div>
+                                                    <div class="-ml-px flex w-0 flex-1">
+                                                        <a href="{{ url('/friend/' . $result->id . '/remove') }}"
+                                                           class="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                                                 height="24" viewBox="0 0 24 24" fill="none"
+                                                                 stroke="currentColor" stroke-width="2"
+                                                                 stroke-linecap="round" stroke-linejoin="round"
+                                                                 class="icon icon-tabler icons-tabler-outline icon-tabler-user-plus">
+                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"/>
+                                                                <path d="M16 19h6"/>
+                                                                <path d="M19 16v6"/>
+                                                                <path d="M6 21v-2a4 4 0 0 1 4 -4h4"/>
+                                                            </svg>
+                                                            Reject
+                                                        </a>
+                                                    </div>
+                                                @else
+                                                    <div class="flex w-0 flex-1">
+                                                        <a href="{{ url('/friend/' . $result->id . '/accept') }}"
+                                                           class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                                                 height="24" viewBox="0 0 24 24" fill="none"
+                                                                 stroke="currentColor" stroke-width="2"
+                                                                 stroke-linecap="round" stroke-linejoin="round"
+                                                                 class="icon icon-tabler icons-tabler-outline icon-tabler-user-minus">
+                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"/>
+                                                                <path
+                                                                    d="M6 21v-2a4 4 0 0 1 4 -4h4c.348 0 .686 .045 1.009 .128"/>
+                                                                <path d="M16 19h6"/>
+                                                            </svg>
+                                                            Add
+                                                        </a>
+                                                    </div>
+                                                @endif
+                                            </div>
                                         </li>
                                     </ul>
                                 @endforeach
-                            @else
                             @endisset
-                            {{--                                                            <a href="#"--}}
-                            {{--                                                               class="flex w-full items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0">View--}}
-                            {{--                                                                all</a>--}}
                         </div>
                     </div>
                 </div>
